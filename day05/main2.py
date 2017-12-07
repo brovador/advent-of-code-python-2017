8a4316bdbec3bc5a1862f6734595657c1d5d5a3b
Jesús Bartús Sancho <jesus.bartus@gmail.com>
2017-12-07 10:59:05 +0100

    🎁 day 05 cleared

---
+++ day05/main2.py
@@ -0,0 +1,33 @@
+#encoding: utf-8
+import os
+
+def main():
+	input_file = './input.txt'
+
+	with open(input_file, 'r') as f:
+		lines = [l.strip() for l in f]
+	
+	jumps = [int(l) for l in lines]
+	pos = 0
+
+	def print_jumps(jumps, pos):
+		values = []
+		for i, j in enumerate(jumps):
+			values.append(str(j) if i != pos else '({0})'.format(j))
+		return ' '.join(values)
+	
+	print_jumps(jumps, pos)
+	i = 0
+	while True:
+		i += 1
+		v = jumps[pos]
+		jumps[pos] += 1 if v < 3 else -1
+		pos += v
+		#print '{0}: {1}'.format(i, print_jumps(jumps, pos))
+		if pos >= len(jumps):
+			break
+	print i
+	
+
+if __name__ == '__main__':
+	main()
