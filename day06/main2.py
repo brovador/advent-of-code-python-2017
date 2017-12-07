d1471ecb460496558dd7b7ec1d184fbe189ddc8c
Jesús Bartús Sancho <jesus.bartus@gmail.com>
2017-12-07 11:15:36 +0100

    🎉 day 06 cleared

---
+++ day06/main2.py
@@ -0,0 +1,37 @@
+#encoding: utf-8
+import os
+
+def main():
+	input_file = './input.txt'
+
+	with open(input_file, 'r') as f:
+		lines = [l.strip() for l in f]
+	
+	banks = [int(x) for x in lines[0].split('\t')]
+
+	states = [banks]
+	i  = 0
+	while True:
+		new_state = [s for s in states[-1]]
+		i += 1
+
+		#1. find highest bank
+		pos = new_state.index(max(new_state))
+		#2. empty selected state
+		blocks = new_state[pos]
+		new_state[pos] = 0
+		#3. distribute banks
+		while blocks > 0:
+			pos = (pos + 1) % len(new_state)
+			new_state[pos] += 1
+			blocks -= 1
+		#print '{0}: {1}'.format(i, ' '.join([str(x) for x in new_state]))
+		if new_state in states:
+			print i - states.index(new_state)
+			break
+		else:
+			states.append(new_state)
+	
+
+if __name__ == '__main__':
+	main()
